781deb02b5745f2c0c9bc8f687f6354d543e1981
Craig Milo Rogers <rogers@isi.edu>
2020-06-14 17:18:11 -0700

    Changes in progress.

--- kgtk/io/kgtkwriter.py
+++ kgtk/io/kgtkwriter.py
@@ -32,9 +32,8 @@ class KgtkWriterMode(Enum):
     NODE = 2 # Enforce node file require columns
     AUTO = 3 # Automatically decide whether to enforce edge or node file required columns
 
-@attr.s(slots=True, frozen=False)
-class KgtkWriter(KgtkBase):
-
+ttr.s(slots=True, frozen=True)
+class KgtkWriterOptions():
     GZIP_QUEUE_SIZE_DEFAULT: int = GzipProcess.GZIP_QUEUE_SIZE_DEFAULT
 
     # TODO: use an enum
@@ -61,11 +60,238 @@ class KgtkWriter(KgtkBase):
     ]
     OUTPUT_FORMAT_DEFAULT: str = OUTPUT_FORMAT_KGTK
 
+    mode: KgtkReaderMode = attr.ib(validator=attr.validators.instance_of(KgtkWriterMode), default=KgtkWriterMode.AUTO)
+
+    column_separator: str = attr.ib(validator=attr.validators.instance_of(str), default=KgtkFormat.COLUMN_SEPARATOR)
+
+    # Require or fill trailing fields?
+    require_all_columns: bool = attr.ib(validator=attr.validators.instance_of(bool), default=True)
+    prohibit_extra_columns: bool = attr.ib(validator=attr.validators.instance_of(bool), default=True)
+    fill_missing_columns: bool = attr.ib(validator=attr.validators.instance_of(bool), default=False)
+
+    # Rename the output columns?
+    #
+    # TODO: use proper validators.
+    output_column_names: typing.Optional[typing.List[str]] = attr.ib(default=None)
+    old_column_names: typing.Optional[typing.List[str]] = attr.ib(default=None)
+    new_column_names: typing.Optional[typing.List[str]] = attr.ib(default=None)
+
+    # How should header errors be processed?
+    header_error_action: ValidationAction = attr.ib(validator=attr.validators.instance_of(ValidationAction), default=ValidationAction.EXIT)
+
+    # Other implementation options?
+    compression_type: typing.Optional[str] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(str)), default=None) # TODO: use an Enum
+    gzip_in_parallel: bool = attr.ib(validator=attr.validators.instance_of(bool), default=False)
+    gzip_queue_size: int = attr.ib(validator=attr.validators.instance_of(int), default=GZIP_QUEUE_SIZE_DEFAULT)
+
+    output_format: str = attr.ib(validator=attr.validators.instance_of(str), default=OUTPUT_FORMAT_DEFAULT) # TODO: use an enum
+
+    @classmethod
+    def add_arguments(cls,
+                      parser: ArgumentParser,
+                      mode_options: bool = False,
+                      default_mode: KgtkWriterMode = KgtkWriterMode.AUTO,
+                      expert: bool = False,
+                      defaults: bool = True,
+                      who: str = "",
+    ):
+
+        # This helper function makes it easy to suppress options from
+        # The help message.  The options are still there, and initialize
+        # what they need to initialize.
+        def h(msg: str)->str:
+            if expert:
+                return msg
+            else:
+                return SUPPRESS
+
+        # This helper function decices whether or not to include defaults
+        # in argument declarations. If we plan to make arguments with
+        # prefixes and fallbacks, the fallbacks (the ones without prefixes)
+        # should get default values, while the prefixed arguments should
+        # not get defaults.
+        #
+        # Note: In obscure circumstances (EnumNameAction, I'm looking at you),
+        # explicitly setting "default=None" may fail, whereas omitting the
+        # "default=" phrase succeeds.
+        #
+        # TODO: continue researching these issues.
+        def d(default: typing.Any)->typing.Mapping[str, typing.Any]:
+            if defaults:
+                return {"default": default}
+            else:
+                return { }
+
+        prefix1: str = "--" if len(who) == 0 else "--" + who + "-"
+        prefix2: str = "" if len(who) == 0 else who + "_"
+        prefix3: str = "" if len(who) == 0 else who + ": "
+        prefix4: str = "" if len(who) == 0 else who + " file "
+
+        fgroup: _ArgumentGroup = parser.add_argument_group(h(prefix3 + "File options"),
+                                                           h("Options affecting " + prefix4 + "processing."))
+        fgroup.add_argument(prefix1 + "column-separator",
+                            dest=prefix2 + "column_separator",
+                            help=h(prefix3 + "Column separator (default=<TAB>)."), # TODO: provide the default with escapes, e.g. \t
+                            type=str, **d(default=KgtkFormat.COLUMN_SEPARATOR))
+
+        # TODO: use an Enum or add choices.
+        fgroup.add_argument(prefix1 + "compression-type",
+                            dest=prefix2 + "compression_type",
+                            help=h(prefix3 + "Specify the compression type (default=%(default)s)."))
+
+        fgroup.add_argument(prefix1 + "error-limit",
+                            dest=prefix2 + "error_limit",
+                            help=h(prefix3 + "The maximum number of errors to report before failing (default=%(default)s)"),
+                            type=int, **d(default=cls.ERROR_LIMIT_DEFAULT))
+
+        fgroup.add_argument(prefix1 + "gzip-in-parallel",
+                            dest=prefix2 + "gzip_in_parallel",
+                            metavar="optional True|False",
+                            help=h(prefix3 + "Execute gzip in parallel (default=%(default)s)."),
+                            type=optional_bool, nargs='?', const=True, **d(default=False))
+
+        fgroup.add_argument(prefix1 + "gzip-queue-size",
+                            dest=prefix2 + "gzip_queue_size",
+                            help=h(prefix3 + "Queue size for parallel gzip (default=%(default)s)."),
+                            type=int, **d(default=cls.GZIP_QUEUE_SIZE_DEFAULT))
+
+        if mode_options:
+            fgroup.add_argument(prefix1 + "mode",
+                                dest=prefix2 + "mode",
+                                help=h(prefix3 + "Determine the KGTK file mode (default=%(default)s)."),
+                                type=KgtkWriterMode, action=EnumNameAction, **d(default_mode))
+            
+        hgroup: _ArgumentGroup = parser.add_argument_group(h(prefix3 + "Header processing"),
+                                                           h("Options affecting " + prefix4 + "header processing."))
+
+        hgroup.add_argument(prefix1 + "header-error-action",
+                            dest=prefix2 + "header_error_action",
+                            help=h(prefix3 + "The action to take when a header error is detected.  Only ERROR or EXIT are supported (default=%(default)s)."),
+                            type=ValidationAction, action=EnumNameAction, **d(default=ValidationAction.EXIT))
+
+        hgroup.add_argument(prefix1 + "output-columns",
+                            dest=prefix2 + "output_column_names",
+                            metavar="NEW_COLUMN_NAME",
+                            help=h(prefix3 + "The list of new column names when renaming all columns. (default=None)"),
+                            type=str, nargs='+')
+
+        hgroup.add_argument(prefix1 + "old-columns",
+                            dest=prefix2 + "old_column_names",
+                            metavar="OLD_COLUMN_NAME",
+                            help=h(prefix3 + "The list of old column names for selective renaming. (default=None)"),
+                            type=str, nargs='+')
+
+        hgroup.add_argument(prefix1 + "new-columns",
+                            dest=prefix2 + "new_column_names",
+                            metavar="NEW_COLUMN_NAME",
+                            help=h(prefix3 + "The list of new column names for selective renaming. (default=None)"),
+                            type=str, nargs='+')
+
+        lgroup: _ArgumentGroup = parser.add_argument_group(h(prefix3 + "Line processing"),
+                                                           h("Options affecting " + prefix4 + "line processing."))
+
+        lgroup.add_argument(prefix1 + "require-all-columns",
+                            dest=prefix2 + "require_all_columms",
+                            metavar="optional True|False",
+                            help=h(prefix3 + "Require all columns on each line. (default=%(default)s)."),
+                            type=optional_bool, nargs='?', const=True, **d(default=False))
+
+        lgroup.add_argument(prefix1 + "prohibit-extra-columns",
+                            dest=prefix2 + "prohibit_extra_columms",
+                            metavar="optional True|False",
+                            help=h(prefix3 + "Prohibit extra columns on each line. (default=%(default)s)."),
+                            type=optional_bool, nargs='?', const=True, **d(default=False))
+
+        lgroup.add_argument(prefix1 + "fill-missing-columns",
+                            dest=prefix2 + "fill_missing_columms",
+                            metavar="optional True|False",
+                            help=h(prefix3 + "Fill missing columns on each line. (default=%(default)s)."),
+                            type=optional_bool, nargs='?', const=True, **d(default=False))
+
+
+    @classmethod
+    # Build the value parsing option structure.
+    def from_dict(cls,
+                  d: dict,
+                  who: str = "",
+                  mode: typing.Optional[KgtkWriterMode] = None,
+                  fallback: bool = False,
+    )->'KgtkWriterOptions':
+        prefix: str = ""   # The destination name prefix.
+        if len(who) > 0:
+            prefix = who + "_"
+
+        # TODO: Figure out how to type check this method.
+        def lookup(name: str, default):
+            prefixed_name = prefix + name
+            if prefixed_name in d and d[prefixed_name] is not None:
+                return d[prefixed_name]
+            elif fallback and name in d and d[name] is not None:
+                return d[name]
+            else:
+                return default
+            
+        writer_mode: KgtkWriterMode
+        if mode is not None:
+            writer_mode = mode
+        else:
+            writer_mode = lookup("mode", KgtkWriterMode.AUTO)
+
+        return cls(
+            mode=writer_mode,
+            column_separator=lookup("column_separator", KgtkFormat.COLUMN_SEPARATOR),
+            require_all_columns=lookup("require_all_columns", True),
+            prohibit_extra_columns=lookup("prohibit_extra_columns", True),
+            fill_missing_columns=lookup("fill_missing_columns", False),
+            header_error_action=lookup("header_error_action", default=ValidationAction.EXIT),
+            compression_type=lookup("compression_type", default=None),
+            gzip_in_parallel=lookup("gzip_in_parallel", default=False),
+            gzip_queue_size=looklup("gzip_queue_size", default=KgtkWriterOptions.GZIP_QUEUE_SIZE_DEFAULT),
+            output_format=lookup("output_format", default=KgtkWriterOptions.OUTPUT_FORMAT_DEFAULT),
+            output_columns=lookup('****************`****************************************************
+        )
+
+    # Build the value parsing option structure.
+    @classmethod
+    def from_args(cls,
+                  args: Namespace,
+                  who: str = "",
+                  mode: typing.Optional[KgtkWriterMode] = None,
+                  fallback: bool = False,
+    )->'KgtkWriterOptions':
+        return cls.from_dict(vars(args), who=who, mode=mode, fallback=fallback)
+
+    def show(self, who: str="", out: typing.TextIO=sys.stderr):
+        prefix: str = "--" if len(who) == 0 else "--" + who + "-"
+        print("%scolumn-separator=%s" % (prefix, repr(self.column_separator)), file=out)
+        if self.compression_type is not None:
+            print("%scompression-type=%s" % (prefix, str(self.compression_type)), file=out)
+        print("%sgzip-in-parallel=%s" % (prefix, str(self.gzip_in_parallel)), file=out)
+        print("%sgzip-queue-size=%s" % (prefix, str(self.gzip_queue_size)), file=out)
+        print("%smode=%s" % (prefix, self.mode.name), file=out)
+        print("%sheader-error-action=%s" % (prefix, self.header_error_action.name), file=out)
+        print("%srequire-all-columns=%s" % (prefix, str(self.require_all_columns)), file=out)
+        print("%sprohibit-extra-columns=%s" % (prefix, str(self.prohibit_extra_columns)), file=out)
+        print("%sfill-missing-columns=%s" % (prefix, str(self.fill_missing_columns)), file=out)
+    
+
+
+DEFAULT_KGTK_WRITER_OPTIONS: KgtkWriterOptions = KgtkReaderOptions()
+
+
+@attr.s(slots=True, frozen=False)
+class KgtkWriter(KgtkBase):
+
     file_path: typing.Optional[Path] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(Path)))
     file_out: typing.TextIO = attr.ib() # Todo: validate TextIO
-    column_separator: str = attr.ib(validator=attr.validators.instance_of(str))
     column_names: typing.List[str] = attr.ib(validator=attr.validators.deep_iterable(member_validator=attr.validators.instance_of(str),
                                                                                      iterable_validator=attr.validators.instance_of(list)))
+
+    options: KgtkWriterOptions = attr.ib()
+
+    # For convenience, the count of columns. This is the same as len(column_names).
+    column_count: int = attr.ib(validator=attr.validators.instance_of(int))
+
     column_name_map: typing.Mapping[str, int] = attr.ib(validator=attr.validators.deep_mapping(key_validator=attr.validators.instance_of(str),
                                                                                                value_validator=attr.validators.instance_of(int)))
 
@@ -75,27 +301,11 @@ class KgtkWriter(KgtkBase):
         attr.ib(validator=attr.validators.deep_iterable(member_validator=attr.validators.instance_of(str),
                                                         iterable_validator=attr.validators.instance_of(list)))
 
-    # For convenience, the count of columns. This is the same as len(column_names).
-    column_count: int = attr.ib(validator=attr.validators.instance_of(int))
-
-    # Require or fill trailing fields?
-    require_all_columns: bool = attr.ib(validator=attr.validators.instance_of(bool))
-    prohibit_extra_columns: bool = attr.ib(validator=attr.validators.instance_of(bool))
-    fill_missing_columns: bool = attr.ib(validator=attr.validators.instance_of(bool))
-
-    # How should header errors be processed?
-    error_file: typing.TextIO = attr.ib(default=sys.stderr)
-    header_error_action: ValidationAction = attr.ib(validator=attr.validators.instance_of(ValidationAction), default=ValidationAction.EXIT)
-
-    # Other implementation options?
-    gzip_in_parallel: bool = attr.ib(validator=attr.validators.instance_of(bool), default=False)
-    gzip_thread: typing.Optional[GzipProcess] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(GzipProcess)), default=None)
-    gzip_queue_size: int = attr.ib(validator=attr.validators.instance_of(int), default=GZIP_QUEUE_SIZE_DEFAULT)
-
-    output_format: str = attr.ib(validator=attr.validators.instance_of(str), default=OUTPUT_FORMAT_DEFAULT) # TODO: use an enum
-
     line_count: int = attr.ib(validator=attr.validators.instance_of(int), default=0)
 
+    gzip_thread: typing.Optional[GzipProcess] = attr.ib(validator=attr.validators.optional(attr.validators.instance_of(GzipProcess)), default=None)
+
+    error_file: typing.TextIO = attr.ib(default=sys.stderr)
     verbose: bool = attr.ib(validator=attr.validators.instance_of(bool), default=False)
     very_verbose: bool = attr.ib(validator=attr.validators.instance_of(bool), default=False)
 
@@ -104,19 +314,9 @@ class KgtkWriter(KgtkBase):
              column_names: typing.List[str],
              file_path: typing.Optional[Path],
              who: str = "output",
-             require_all_columns: bool = True,
-             prohibit_extra_columns: bool = True,
-             fill_missing_columns: bool = False,
              error_file: typing.TextIO = sys.stderr,
-             header_error_action: ValidationAction = ValidationAction.EXIT,
-             gzip_in_parallel: bool = False,
-             gzip_queue_size: int = GZIP_QUEUE_SIZE_DEFAULT,
-             column_separator: str = KgtkFormat.COLUMN_SEPARATOR,
              mode: typing.Union[KgtkReaderMode, KgtkWriterMode] = KgtkWriterMode.AUTO,
-             output_format: typing.Optional[str] = None,
-             output_column_names: typing.Optional[typing.List[str]] = None,
-             old_column_names: typing.Optional[typing.List[str]] = None,
-             new_column_names: typing.Optional[typing.List[str]] = None,
+             options: typing.Optional[KgtkWriterOptions] = None,
              verbose: bool = False,
              very_verbose: bool = False)->"KgtkWriter":
 
@@ -127,6 +327,9 @@ class KgtkWriter(KgtkBase):
             wmode = mode
         elif isinstance(mode, KgtkReaderMode):
             wmode = KgtkWriterMode[mode.name]
+
+        if options is None:
+            options = DEFAULT_KGTK_WRITER_OPTIONS
 
         if file_path is None or str(file_path) == "-":
             if verbose:
